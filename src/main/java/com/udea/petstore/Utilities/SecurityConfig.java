ffad1b20db0eea0060d92875417111b9119dde0a
Leandro <leandro.toro@udea.edu.co>
2025-06-11 11:29:35 -0500

    Se activa JWT

--- src/main/java/com/udea/petstore/Utilities/SecurityConfig.java
+++ src/main/java/com/udea/petstore/Utilities/SecurityConfig.java
@@ -4,11 +4,13 @@ import com.udea.petstore.Auth.Config.JwtAuthenticationFilter;
 import com.udea.petstore.Auth.Config.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
